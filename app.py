7b897af848a3603dcecde93d9b1cc762fa78f4c9
josue <josue.medina9991@gmail.com>
2024-04-06 12:10:15 -0600

    ytdl discord bot with oop

--- app.py
+++ app.py
@@ -1,31 +1,9 @@
-# import bot, os, asyncio
-import discord
-from decouple import config
+from bot import LactomedaBot
 
 def main():
-    # bot.runDiscordBot()
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents=intents)
-    
-    @client.event
-    async def on_ready():
-        print(f'We have logged in as {client.user}')
-        
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-        if message.author.name in ["zeropatos","zer0woo"]:
-                print("[+] She is pretty as f")
-                await message.add_reaction("❤️")
-        if message.content.startswith('>l'):
-            await message.channel.send(f'Fk mamon el {message.author.name}')
-            
-    
-    client.run(config('tk'))
-
-
+    lactomeda = LactomedaBot()
+    lactomeda.runBot()
+   
 if __name__ == "__main__":
     main()
 
--- bot.py
+++ bot.py
@@ -1,37 +1,60 @@
-import responses
-from discord import Intents,Client, Message
+import discord, os, asyncio, yt_dlp
 from decouple import config
+from collections import deque
 
 
+class LactomedaBot():
+    def __init__(self):
+        self.listSongs = []
+        self.queuSongs = deque(self.listSongs)
+        
+    async def joinToMusicYt(self,message,voiceClients,ytdl,ffmpeg_options):
+        try:
+            voiceClient = await message.author.voice.channel.connect()
+            voiceClients[voiceClient.guild.id] = voiceClient
+            
+        except Exception as e:
+            
+            print(f"[-] There was an error of type: {e}")
+        try:
+            # print(str(message.content).split()[1])
+            url = message.content.split()[1]
+            self.queuSongs.append(url)
+            print(self.queuSongs)
+            loop = asyncio.get_event_loop()
+            data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url,download=False))
+            song = data["url"]
+            print(f"[+] {data}")
+            player = discord.FFmpegPCMAudio(song, **ffmpeg_options)
+            voiceClients[message.guild.id].play(player)
+            # print("Muac")
+        except Exception as e:
+            print('An exception occurred')
 
-async def sendMsg(msg,userMsg,isPriv):
-    try:
-      response = responses.handleRes(userMsg)
-      await msg.author.send(response) if isPriv else await msg.channel.send(response)
-    except Exception as e:
-      print(f'An exception occurred: {e}')
-      
-      
-def runDiscordBot():
-    tk =  config('tk')
-    
-    intents: Intents = Intents.default()
-    intents.message_content = True
-    client: Client = Client(intents=intents)
-    
-    @client.event
-    async def onReady() -> None:
-        print(f"{client.user} is now running! ")
-    
-    @client.event
-    async def onMsg(msg : Message) -> None:
-        if msg.author == client.user:
-            return
-        username: str = str(msg.author)
-        userMsg: str = msg.content
-        channel: str = str(msg.channel)
+    def runBot(self):
+        voiceClients = {}
+        yt_dlp_options = {"format" : "bestaudio/best"}
+        ytdl = yt_dlp.YoutubeDL(yt_dlp_options)
         
+        ffmpeg_options = {'options':"-vn"}
         
-        await sendMsg(msg,userMsg)
+         # bot.runDiscordBot()
+        intents = discord.Intents.default()
+        intents.message_content = True
+        client = discord.Client(intents=intents)
         
-    client.run(tk)
+        @client.event
+        async def on_ready():
+            print(f'We have logged in as {client.user}')
+            
+        @client.event
+        async def on_message(message):
+            if message.author == client.user:
+                return
+            if message.author.name in ["zeropatos","zer0woo","megustaahri"]:
+                    print("[+] She is pretty as f")
+                    await message.add_reaction("❤️")
+            if message.content.startswith('-p ') or message.content.startswith('-P '):
+               await self.joinToMusicYt(message=message,voiceClients=voiceClients,ytdl=ytdl,ffmpeg_options=ffmpeg_options)
+        
+        client.run(config('tk'))
