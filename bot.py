17107a19cdd9ef76e12f6e2102c7ccf5e2391c63
josue <josue.medina9991@gmail.com>
2024-05-01 19:47:39 -0600

    features

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
-import discord, os, asyncio, yt_dlp
+import discord, asyncio, yt_dlp
 from decouple import config
 from collections import deque
+from discord.ext import commands
 
 
 class LactomedaBot():
@@ -8,35 +9,53 @@ class LactomedaBot():
         self.listSongs = []
         self.queuSongs = deque(self.listSongs)
         
-    async def joinToMusicYt(self,message,voiceClients,ytdl,ffmpeg_options):
+    # @commands.command()
+    async def joinVc(self,message,voiceClients):
         try:
-            voiceClient = await message.author.voice.channel.connect()
-            voiceClients[voiceClient.guild.id] = voiceClient
+            if message.author.voice is None:
+                await message.channel.send("No estas en un canal de voz mocoso")
+                return
             
+            if message.guild.voice_client is None:
+                voiceClient = await message.author.voice.channel.connect()
+                voiceClients[voiceClient.guild.id] = voiceClient
+                print("[+] Connected to voice channel")
+                return True
+                
         except Exception as e:
-            
-            print(f"[-] There was an error of type: {e}")
+            print(f"[-] There was an error while joining a voice channel: {e}")
+        
+        
+        
+    async def musicYt(self,message,voiceClients,ffmpeg_options):
         try:
+            yt_dlp_options = {"format" : "bestaudio"}
             # print(str(message.content).split()[1])
+            jVc = await self.joinVc(message,voiceClients)
+            if jVc is None:
+                return
             url = message.content.split()[1]
             self.queuSongs.append(url)
             print(self.queuSongs)
-            loop = asyncio.get_event_loop()
-            data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url,download=False))
-            song = data["url"]
-            print(f"[+] {data}")
-            player = discord.FFmpegPCMAudio(song, **ffmpeg_options)
-            voiceClients[message.guild.id].play(player)
-            # print("Muac")
+            
+            with yt_dlp.YoutubeDL(yt_dlp_options) as ytdl:
+                loop = asyncio.get_event_loop()
+                data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url,download=False))
+                song = data["url"]
+                voiceClient = voiceClients[message.guild.id]
+                if voiceClient is not None:
+                    player = discord.FFmpegPCMAudio(song, **ffmpeg_options)
+                    voiceClient.play(player, after=lambda e: print(f"[-] Player error: {e}"))
+                else:
+                    print("[+] Lactomeda is already playing something")
         except Exception as e:
-            print('An exception occurred')
+            print(f'[-] An exception occurred: {e}')
 
     def runBot(self):
         voiceClients = {}
-        yt_dlp_options = {"format" : "bestaudio/best"}
-        ytdl = yt_dlp.YoutubeDL(yt_dlp_options)
+       
         
-        ffmpeg_options = {'options':"-vn"}
+        ffmpeg_options = {'before_options':'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options':"-vn"}
         
          # bot.runDiscordBot()
         intents = discord.Intents.default()
@@ -55,6 +74,6 @@ class LactomedaBot():
                     print("[+] She is pretty as f")
                     await message.add_reaction("❤️")
             if message.content.startswith('-p ') or message.content.startswith('-P '):
-               await self.joinToMusicYt(message=message,voiceClients=voiceClients,ytdl=ytdl,ffmpeg_options=ffmpeg_options)
+               await self.musicYt(message=message,voiceClients=voiceClients,ffmpeg_options=ffmpeg_options)
         
         client.run(config('tk'))
